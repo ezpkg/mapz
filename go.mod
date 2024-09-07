84c730ce36a1861bd5cba64fdef94515d0f0e3fd
Oliver N <olvrng@gmail.com>
2024-09-07 11:02:37 +0700

    v0.1.2

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module ezpkg.io/mapz
 
 go 1.21
 
+require (
+	ezpkg.io/typez v0.1.2
+)
+
 require golang.org/x/exp v0.0.0-20240707233637-46b078467d37
--- mapz.go
+++ mapz.go
@@ -5,6 +5,8 @@ import (
 
 	"golang.org/x/exp/constraints"
 	"golang.org/x/exp/maps"
+
+	"ezpkg.io/typez"
 )
 
 func FromSliceFunc[S ~[]E, E comparable, R any](s S, fn func(E) R) map[E]R {
@@ -29,4 +31,90 @@ func SortedKeysAndValues[M ~map[K]V, K constraints.Ordered, V any](m M) ([]K, []
 		values[i] = m[key]
 	}
 	return keys, values
+}
+
+func Append[M ~map[K]V, K comparable, V any](m M, items map[K]V) map[K]V {
+	if m == nil {
+		m = make(map[K]V, len(items))
+	}
+	for k, v := range items {
+		m[k] = v
+	}
+	return m
+}
+
+func Merge[M ~map[K]V, K comparable, V any](maps ...M) map[K]V {
+	N := 0
+	for _, x := range maps {
+		N += len(x)
+	}
+	m := make(map[K]V, N)
+	for _, x := range maps {
+		for k, v := range x {
+			m[k] = v
+		}
+	}
+	return m
+}
+
+func Exists[M ~map[K]V, K comparable, V any](m M, key K) bool {
+	_, ok := m[key]
+	return ok
+}
+
+func ExistsFunc[M ~map[K]V, K comparable, V any](m M, fn func(K, V) bool) bool {
+	for k, v := range m {
+		if fn(k, v) {
+			return true
+		}
+	}
+	return false
+}
+
+func ExistsAll[M ~map[K]V, K comparable, V any](m M, keys ...K) bool {
+	for _, key := range keys {
+		if !Exists(m, key) {
+			return false
+		}
+	}
+	return true
+}
+
+func ExistsAny[M ~map[K]V, K comparable, V any](m M, keys ...K) bool {
+	for _, key := range keys {
+		if Exists(m, key) {
+			return true
+		}
+	}
+	return false
+}
+
+func FilterFunc[M ~map[K]V, K comparable, V any](m M, fn func(K, V) bool) map[K]V {
+	result := make(map[K]V, len(m))
+	for k, v := range m {
+		if fn(k, v) {
+			result[k] = v
+		}
+	}
+	return result
+}
+
+func FilterKeys[M ~map[K]V, K comparable, V any](m M, keys ...K) map[K]V {
+	result := make(map[K]V, len(m))
+	for _, k := range keys {
+		if v, ok := m[k]; ok {
+			result[k] = v
+		}
+	}
+	return result
+}
+
+func FilterValues[M ~map[K]V, K, V comparable](m M, values ...V) map[K]V {
+	result := make(map[K]V, len(m))
+	for k, v := range m {
+		if typez.In(v, values...) {
+			result[k] = v
+		}
+	}
+	return result
 }
